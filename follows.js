9170604d169d3361ebf9be1a1b5816368db8555e
rulen <rulen12@googlemail.com>
2015-01-08 12:35:16 +0000

    Update follows.js
    Added read-only key and feed id

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'ttgO0rlkRQClsL3KdasAaLYZytalsVVX0vp6GGMmdCc0fydt', // My read-only key
+		defaultFeeds	= [77050], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
